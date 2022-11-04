8360efea735476d232f7b53fcec258e779b69919
Caio <caiusito@gmail.com>
2022-11-04 19:36:18 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND = 404;
+// const INTERNAL_SERVER_ERROR = 500;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -20,6 +22,17 @@ app.listen(PORT, () => {
 app.get('/talker', async (req, res) => {
   const talkers = await readTalkersInfo();
 
-  if (talkers.length === 0) return res.status(200).json([]);
-  return res.status(200).json(talkers);
+  if (talkers.length === 0) return res.status(HTTP_OK_STATUS).json([]);
+  return res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await readTalkersInfo();
+  const talkerId = Number(req.params.id);
+  const talker = talkers.find((t) => t.id === talkerId);
+  if (talker) { 
+    return res.status(HTTP_OK_STATUS).json(talker); 
+}
+  return res.status(NOT_FOUND).json({ 
+    message: 'Pessoa palestrante não encontrada' });
 });
