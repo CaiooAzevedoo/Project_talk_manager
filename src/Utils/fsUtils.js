a8fa068d6d0628be6faa58fd7e99cf2e3bf2216b
Caio <caiusito@gmail.com>
2022-11-04 22:39:43 -0300

    Desenvolvendo o req 4

--- src/Utils/fsUtils.js
+++ src/Utils/fsUtils.js
@@ -14,8 +14,25 @@ async function readTalkersInfo() {
 
 function randomToken() {
     return crypto.randomBytes(8).toString('hex');
-  }
+}
+
+function validateEmail(email) {
+    if (!email) { 
+        return 'O campo "email" é obrigatório'; 
+}
+    if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) { 
+        // https://regexr.com/3e48o //
+        return 'O "email" deve ter o formato "email@email.com"'; 
+}
+}
+  
+function validatePassword(password) {
+    if (!password) return 'O campo "password" é obrigatório';
+    if (password.length < 6) return 'O "password" deve ter pelo menos 6 caracteres';
+}
 
 module.exports = { 
 readTalkersInfo,
-randomToken };
+randomToken,
+validateEmail,
+validatePassword };
